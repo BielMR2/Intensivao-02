0cab72adfe0112ee69d005f0b1d4e170c6ddb4b9
BielMR2 <gabrielmribeiro2007@outlook.com>
2023-08-07 16:13:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log("Hello World!")
+
+var num1 = 5
+var num2 = 2
+console.log(num1 + num2)
+
+var num3 = 5
+if(typeof num3 == "number") {
+    console.log("É um número")
+} else {
+    console.log("Não é um número")
+}
+
+if(typeof num3 == "string") {
+    console.log("É uma string")
+} else {
+    console.log("Não é uma string")
+}
+
+var booleano = false
+
+if (booleano == true || !false) {
+    console.log("É um booleano")
+} else {
+    console.log("Não é um booleano")
+}
+
+console.log(num1 - num2)
+
+console.log(num1 * num2)
+
+console.log(num1 / num2)
+
+if (num1 % 2 == 0) {
+    console.log("É um número par")
+} else {
+    console.log("Não é um número par")
+}
+
+if (!(num1 % 2 == 0)) {
+    console.log("É um número par")
+} else {
+    console.log("Não é um número par")
+}
+
